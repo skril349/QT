f56710794ffaf336bb43e6f75a246ea0b3924656
toniprotofy <toni@protofy.xyz>
2022-11-19 19:12:50 +0100

    pointers herarchy

--- memoryManagment/MemoryManagment/main.cpp
+++ memoryManagment/MemoryManagment/main.cpp
@@ -21,6 +21,14 @@ void do_stuff(){
     //Shared pointer
     std::shared_ptr<House> up_house_h2 {new House(nullptr,"Second")};
     up_house_h2->print_info();
+
+    //we re going to create parent pointer and when this is going to be destroyed, it will destroy eery child pointer.
+
+    House* p_parent = new House(nullptr,"Parent");
+    House* phouse1 = new House(p_parent, "House1");
+    House* phouse2 = new House(phouse1, "House2");
+
+    delete p_parent;
 }
 
 int main(int argc, char *argv[])
